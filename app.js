7570c818f1df46ced31beb1a46777aa71929336c
J3 <gitlab@nevs.me>
2023-09-27 16:36:47 +0200

    parse data on server side and handle tallys

--- app.js
+++ app.js
@@ -21,19 +21,53 @@ wss.on('connection', (ws) => {
   ws.send(JSON.stringify(receivedData));
 });
 
+let lastId = null;
+let lastSource = null;
+let tallyCount = 0;
+
 app.post('/webhook', upload.any(), (req, res) => {
   const payload = {
     files: req.files,
     body: JSON.parse(req.body.payload_json)
   };
 
-  receivedData.push(payload);
+  const currentSource = payload.body.extra.source;
+  const timestamp = new Date(payload.body.embeds[0].timestamp).toLocaleString();
 
-  console.log('Received data', payload);
+  if (lastSource === currentSource) {
+    tallyCount++;
+  } else {
+    tallyCount = 1;
+    lastId = null; // Reset the lastId since the streak is broken
+    lastSource = currentSource;
+  }
+
+  const titleText = `I killed ${tallyCount} ${currentSource}${tallyCount > 1 ? 's' : ''}.`;
+  const displayText = `${titleText} (${timestamp})`;
+
+  const newData = {
+    id: lastId || payload.body.embeds[0].timestamp,
+    currentSource,
+    timestamp,
+    displayText,
+    titleText,
+    tallyCount
+  };
+
+  if (lastId) {
+    const index = receivedData.findIndex(item => item.id === lastId);
+    if (index !== -1) {
+      receivedData[index] = newData;
+    }
+  } else {
+    receivedData.push(newData);
+    lastId = newData.id;
+  }
+
   if (receivedData.length > 10) {
     receivedData.shift();
   }
-  
+
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(receivedData));
@@ -49,15 +83,15 @@ app.get('/log', (req, res) => {
 
 
 app.get('/fetchHiscores', async (req, res) => {
-    const player = req.query.player;
-    try {
-      const response = await hiscores.getStats(player);
-      console.log(response);
-      res.send(response);
-    } catch (error) {
-      res.status(500).send('Error fetching hiscores');
-    }
-  });
+  const player = req.query.player;
+  try {
+    const response = await hiscores.getStats(player);
+    console.log(response);
+    res.send(response);
+  } catch (error) {
+    res.status(500).send('Error fetching hiscores');
+  }
+});
 
 server.listen(3000, () => {
   console.log('Server started on http://localhost:3000/');
