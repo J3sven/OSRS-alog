0cf9b52473d5824f8008d54f4d9b94dd46bb040d
J3 <gitlab@nevs.me>
2023-09-29 00:04:40 +0200

    fix: log bug fixed fr fr no cap

--- js/server/store.js
+++ js/server/store.js
@@ -1,41 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-function storePayload(playerName, type, processedData) {
-    const sanitizedPlayerName = playerName.replace(/ /g, '_');
-    const folderPath = path.join(__dirname, '..', '..', 'data', sanitizedPlayerName);
+function sanitizePlayerName(playerName) {
+    return playerName.replace(/ /g, '_');
+}
+
+function getFilePaths(playerName, type) {
+    const folderPath = path.join(__dirname, '..', '..', 'data', playerName);
     const logFilePath = path.join(folderPath, 'log.json');
     const typeFilePath = path.join(folderPath, `${type.toLowerCase()}.json`);
+    return { folderPath, logFilePath, typeFilePath };
+}
 
+function ensureFolderExists(folderPath) {
     if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath, { recursive: true });
     }
+}
 
-    let logData = [];
-    if (fs.existsSync(logFilePath)) {
-        const fileContent = fs.readFileSync(logFilePath, 'utf-8');
-        logData = fileContent ? JSON.parse(fileContent) : [];
+function readJSONFile(filePath) {
+    if (fs.existsSync(filePath)) {
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        return fileContent ? JSON.parse(fileContent) : [];
     }
-
-    let existingTypeData = [];
-    if (fs.existsSync(typeFilePath)) {
-        existingTypeData = JSON.parse(fs.readFileSync(typeFilePath));
-    }
-
-    const updatedTypeData = updateData(existingTypeData, processedData);
-    fs.writeFileSync(typeFilePath, JSON.stringify(updatedTypeData, null, 2));
-
-    const existingLogIndex = logData.findIndex(
-        item => item.type === type && item.id === processedData.id
-    );
-
-    if (existingLogIndex === -1) {
-        logData.push({ type, ...processedData });
-    } else {
-        logData[existingLogIndex] = { type, ...processedData };
-    }
-
-    fs.writeFileSync(logFilePath, JSON.stringify(logData, null, 2));
+    return [];
 }
 
 function updateData(existingData, newData) {
@@ -51,6 +39,39 @@ function updateData(existingData, newData) {
 
     return existingData;
 }
+
+function updateLogData(logData, type, processedData) {
+    const latestTypeIndex = logData.reduce((acc, curr, idx) => {
+        return (curr.type === type) ? idx : acc;
+    }, -1);
+
+    if (latestTypeIndex >= 0 && logData[latestTypeIndex].currentSource === processedData.currentSource) {
+        processedData.tallyCount = logData[latestTypeIndex].tallyCount + 1;
+        logData[latestTypeIndex] = { type, ...processedData };
+    } else {
+        processedData.tallyCount = 1;
+        logData.push({ type, ...processedData });
+    }
+
+    return logData;
+}
+
+function storePayload(playerName, type, processedData) {
+    const sanitizedPlayerName = sanitizePlayerName(playerName);
+    const { folderPath, logFilePath, typeFilePath } = getFilePaths(sanitizedPlayerName, type);
+
+    ensureFolderExists(folderPath);
+
+    let logData = readJSONFile(logFilePath);
+    let existingTypeData = readJSONFile(typeFilePath);
+
+    const updatedTypeData = updateData(existingTypeData, processedData);
+    fs.writeFileSync(typeFilePath, JSON.stringify(updatedTypeData, null, 2));
+
+    const updatedLogData = updateLogData(logData, type, processedData);
+    fs.writeFileSync(logFilePath, JSON.stringify(updatedLogData, null, 2));
+}
+
 
 module.exports = {
     storePayload
