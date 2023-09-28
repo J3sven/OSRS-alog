2a2a3076c6a6efb136af0b734831ba9a25c59c2f
J3 <gitlab@nevs.me>
2023-09-28 19:58:13 +0200

    further frontend adjustments

--- js/client/websocket.js
+++ js/client/websocket.js
@@ -5,7 +5,7 @@ function markdownToHtml(markdown) {
 }
 
 let lastSource = null;
-let firstEvent = true;
+let isFirstFetch = true;
 const logs = {};
 
 ws.addEventListener('message', (event) => {
@@ -21,7 +21,8 @@ ws.addEventListener('message', (event) => {
 async function fetchAndUpdateLogs(playerName) {
   try {
     // Construct the URL to fetch
-    const url = `/data/${playerName}/log.json`;
+    const sanitizedPlayerName = playerName.replace(/ /g, '_');
+    const url = `/data/${sanitizedPlayerName}/log.json`;
 
     // Fetch the log.json file
     const response = await fetch(url);
@@ -29,34 +30,68 @@ async function fetchAndUpdateLogs(playerName) {
     // Parse the JSON response
     const logsData = await response.json();
 
-    // Clear existing logs (if you wish)
+    // Sort logsData if necessary
+    logsData.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+
+    // Get logsElement
     const logsElement = document.getElementById('RAAccordion');
-    logsElement.innerHTML = '';
+
+    if (isFirstFetch && logsData.length > 0) {
+      logsElement.innerHTML = '';
+      isFirstFetch = false;
+    }
 
     // Update UI based on logsData
-    logsData.forEach((data) => {
+    logsData.forEach((data, index) => {
       const id = data.id;
       const titleText = data.titleText;
       const displayText = data.displayText;
-
-      // Create new elements for each log entry and append them
-      const newHeader = document.createElement('a');
-      newHeader.className = 'AMHead ui-accordion-header ui-helper-reset ui-state-default ui-corner-all';
-      newHeader.id = 'A' + id;
-      newHeader.innerHTML = `<span class="ui-icon ui-icon-triangle-1-e"></span><span class="Title">${titleText}</span>`;
-
-      const newBody = document.createElement('div');
-      newBody.className = 'AMBody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom';
-      newBody.id = 'A' + id + 'Body';
-      newBody.style.height = '0px';
-      newBody.innerHTML = `<div class="AMBodyLiner"><p>${displayText}</p></div>`;
-
-      logsElement.appendChild(newHeader);
-      logsElement.appendChild(newBody);
+    
+      const existingHeader = document.getElementById('A' + id);
+      const existingBody = document.getElementById('A' + id + 'Body');
+    
+      if (existingHeader && existingBody) {
+        // Update existing elements if they exist
+        existingHeader.innerHTML = `<span class="ui-icon ui-icon-triangle-1-e"></span><span class="Title">${titleText}</span>`;
+        existingBody.innerHTML = `<div class="AMBodyLiner"><p>${displayText}</p></div>`;
+      } else {
+        // Create new elements if they don't exist
+        const newHeader = document.createElement('a');
+        newHeader.className = 'AMHead ui-accordion-header ui-helper-reset ui-state-default ui-corner-all';
+        newHeader.id = 'A' + id;
+        newHeader.innerHTML = `<span class="ui-icon ui-icon-triangle-1-e"></span><span class="Title">${titleText}</span>`;
+    
+        const newBody = document.createElement('div');
+        newBody.className = 'AMBody ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom';
+        newBody.id = 'A' + id + 'Body';
+        newBody.style.height = '0px';
+        newBody.innerHTML = `<div class="AMBodyLiner"><p>${displayText}</p></div>`;
+    
+        // Insert new elements at the beginning of logsElement
+        if (logsElement.firstChild) {
+          logsElement.insertBefore(newBody, logsElement.firstChild);
+          logsElement.insertBefore(newHeader, logsElement.firstChild);
+        } else {
+          logsElement.appendChild(newHeader);
+          logsElement.appendChild(newBody);
+        }
+    
+        if (index === 0) {
+          expandNewlyAddedElement(newHeader);
+        }
+      }
+    
+      logs[id] = data;
     });
+    
 
   } catch (error) {
     // Handle errors
     console.error('Failed to fetch and update logs:', error);
   }
 }
+
+window.onload = async function() {
+  const playerName = 'J3_gg'; // Replace this with how you get the playerName
+  await fetchAndUpdateLogs(playerName);
+};
